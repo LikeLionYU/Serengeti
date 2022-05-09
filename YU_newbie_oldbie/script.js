2e9c044367ab78d31b1806646d00872afa74b4b3
nkyungm <skarudals27@naver.com>
2022-05-09 21:00:57 +0900

    script

--- YU_newbie_oldbie/script.js
+++ YU_newbie_oldbie/script.js
@@ -40,20 +40,20 @@ const result = {
     "first_grade": {
         "person": "아무것도 모르는 순수한 새내기",
         "explain": "캠퍼스라이프를 막 시작하면서 캠퍼스의 풍경들, 사람들, 교수님들, 강의실, 축제, 동아리 모든 게 신기해요! 영남대는 왜 이렇게 큰지 길을 헤매다가 지도를 보고 강의실에 겨우 도착하기도 했구요. 입고싶은 옷도 마음껏 입고, 술도 마음껏 마실거예요!",
-        "img": "lion.jpg"
+        "img": "first_grade.png"
     },
     "second_grade": { "person": "조금씩 적응해가는 영대인",
      "explain": "대학교에 들어와서 캠퍼스 라이프에 점점 적응해가고 있어요. 이젠 눈 감고도 강의실을 찾아갈 수 있을 것 같아요! 캠퍼스에 대한 환상도 점점 걷혀가지만 우리에겐 종강이 있으니까요.",
-      "img": "lion.jpg" },
+      "img": "second_grade.jpg" },
     "third_grade": { "person": "아무것도 모르는 뉴비가 귀여운 고학년 학교 시설을 통달하신 당신",
      "explain": "분명 영남대가 되게 크게 느껴졌는데 지금은 그렇게 큰 것 같지도 않아요. 내가 영남대에서 필요한 것이 어디에 있는지 다 파악하고 있으니까요. 오늘도 옷은 최대한 편하게 입고, 모자도 푹 눌러쓰고 학교를 가요. 혹시 과잠에 대한 미련도 점점 사라져가고 있진 않으신가요?",
-      "img": "lion.jpg" },
+      "img": "thirth.png"},
     "fourth_grade": { "person": "모든 것을 통달한 올드비",
      "explain": "나는 가만히 학교다닌 죄밖에 없는데 사람들이 나를 화석이라 부르기 시작해서 속상해요. 학년만 높지 아직 어린데 동아리 모집에 저는 부르지 않아서 속상해요.",
-      "img": "lion.jpg" },
+      "img": "fourth.png" },
     "fifth_grade": { "person": "캠퍼스의 역사를 경험한 화석",
      "explain": "영대에 많은 것이 사라지고 생기는 것을 두 눈으로 본 당신. 혹시 학번을 말했을 때 주변사람들이 다들 놀라지 않던가요?",
-      "img": "lion.jpg" }
+      "img": "fifth_grade.jpg" }
 }
 
 
@@ -69,21 +69,25 @@ titleBtn.addEventListener('click', () => {
 
 aBtn.addEventListener('click', () => {
     switch (type.innerHTML) {
-        case 'EI':
+        case 'first_grade':
             let e = parseInt(EI.value);
             EI.setAttribute('value', e + 1);
             break;
-        case 'SN':
+        case 'second_grade':
             let s = parseInt(SN.value);
             SN.setAttribute('value', s + 1);
             break;
-        case 'TF':
+        case 'third_grade':
             let t = parseInt(TF.value);
             TF.setAttribute('value', t + 1);
             break;
-        case 'JP':
+        case 'fourth_grade':
             let j = parseInt(JP.value);
             JP.setAttribute('value', j + 1);
+            break;
+        case 'fifth_grade':
+            let a = parseInt(JP.value);
+            JP.setAttribute('value', a + 1);
             break;
     }
     updateQuestion();
